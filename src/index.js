9180039bc18dfcd8d681e67d9e3537089fa0df0f
AllCoolNicknamesWereTaken <oliwia_bar@wp.pl>
2017-11-12 02:22:21 +0100

    Rewriting started

--- src/index.js
+++ src/index.js
@@ -2,8 +2,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Helmet} from "react-helmet";
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user1 = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const user2 = {
+  firstName: 'Oliwia',
+  lastName: 'Bar'
+};
+
+
+
+function getGreeting() {
+  const tab = [];
+  for(let i = 0; i<= 10; i++) {
+    if(i % 2 == 0) {
+      tab[i] = <h1>{formatName(user2)}</h1>;
+    }else{
+      tab[i] = <h1>{formatName(user1)}</h1>;
+    }
+  }
+  return tab;
+}
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//       <p>dupa</p>
+//       </div>
+//   );
+//
+//   class Welcome extends React.Component {
+//     render() {
+//       return <h1> Hello, {this.props.name}</h1>;
+//     }
+//   }
+
+
+function Head(props) {
+  return (
+    <Helmet>
+      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+      <title>Focused Logowanie</title>
+      <link rel="stylesheet" href="main.css"/>
+      <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Dancing+Script" rel="stylesheet"/>
+    </Helmet>
+  );
+};
+
+
+
+  function Belt(props) {
+    return (
+      <div class='belt'>
+          <div class ='headings'>
+            Focused
+          </div>
+        </div>
+      );
+  };
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Head />
+        <Belt />
+      </div>
+    );
+  }
+}
+
+  const element = <Belt name="Sara" a="hej" b="czesc" />;
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
