ea5d6bf7402061e654ba9cf6553836164564a6c0
AllCoolNicknamesWereTaken <oliwia_bar@wp.pl>
2017-12-02 21:25:20 +0100

    database configuration

--- srv/index.js
+++ srv/index.js
@@ -1,15 +1,35 @@
 
-
+var config = require('./config.json');
 var Hapi = require("hapi");
+var DB = require("mysql");
 
+var db = DB.createConnection({
+  host      : config.DB.host,
+  user      : config.DB.user,
+  password  : config.DB.pass,
+  database  : config.DB.db
+
+});
+db.connect();
 var routes = [{
   path: "/events",
   handler: function(request, reply) {
-    reply({
-      id: 1,
-      title: 'hello',
-      start: new Date()
+    db.query("SELECT * FROM events", function(err, results) {
+      if (err) {
+        reply({
+          status: 'error',
+          error: err
+        });
+        return;
+      }
+      console.log(results);
+      reply({
+        status: 'ok',
+        data: results
+      })
     });
+    // reply({
+    // });
   }
 }, {
   path: "/oliwka",
