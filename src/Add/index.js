75c76f8f5d1f7df5df9f2a4f304f7a873a938f27
Oliwia Bar <oliwiabar@Oliwias-MacBook-Air.local>
2017-12-28 14:18:21 +0100

    fixes

--- src/Add/index.js
+++ src/Add/index.js
@@ -9,6 +9,7 @@ export class Add extends React.Component {
         super(props);
         console.log(props);
         this.state = {
+          eventName: '',
           startTime: '',
           endTime: '',
           startDate: props.start,
@@ -18,7 +19,61 @@ export class Add extends React.Component {
         };
 
         this.send = this.send.bind(this);
+        this.updatecomment = this.send.bind(this);
+        this.updatestartDate =this.updatestartDate.bind(this);
+        this.updateendDate =this.updateendDate.bind(this);
+        this.updatestartTime =this.updatestartTime.bind(this);
+        this.updateeventName =this.updateeventName.bind(this);
+        this.updateendTime =this.updateendTime.bind(this);
       }
+
+      send = function () {
+        fetch('http://localhost:8080/addevents', { method: 'POST', body: JSON.stringify({
+          title:  this.state.eventName,
+          start:  this.state.startDate,
+          end:    this.state.endDate
+        }) })
+        .then(function() {
+          window.location.hash = '#main';
+        });
+
+      }
+
+      updatestartDate = function(evt) {
+        this.setState({
+          startDate: evt.target.value
+        });
+      }
+      updateendDate = function(evt) {
+        this.setState({
+          endDate: evt.target.value
+        });
+        console.log('e?', this.state.endDate)
+      }
+      updatestartTime = function(evt) {
+        console.log(evt.target.value);
+        this.setState({
+          startTime: evt.target.value
+        });
+        console.log('2 ', this.state.startTime);
+      }
+      updatecomment = function(evt) {
+        this.setState({
+          comment: evt.target.value
+        });
+      }
+      updateeventName = function(evt) {
+        this.setState({
+          eventName: evt.target.value
+        });
+
+      }
+      updateendTime = function(evt) {
+          this.setState({
+            endTime: evt.target.value
+          });
+          console.log('end ', this.state.endTime);
+        }
 
     render = function() {
     return (
@@ -34,22 +89,22 @@ export class Add extends React.Component {
 
           <div class="time">
             <input type="time" id="eventStartTime" name="name" placeholder="Godzina rozpoczęcia"
-            value={this.state.startTime} onChange={evt => this.updatestartTime(evt)} />
+            value={this.state.startTime} onChange={this.updatestartTime} />
             -
             <input type="time" id="eventEndTime" name="name" placeholder="godzina zakończenia"
-            value={this.state.endTime} onChange={evt => this.updateendTime(evt)} />
+            value={this.state.endTime} onChange={this.updateendTime} />
           </div>
           <div class="time">
             <input type="date" id="eventStartDate" name="name" placeholder="od"
-             value={moment(this.state.startDate).format('YYYY-MM-DD')} onChange={evt => this.updatestartDate(evt)} />
+             value={this.state.startDate} onChange={this.updatestartDate} />
             -
             <input type="date" id="eventEndDate" name="name" placeholder="do"
-            value={moment(this.state.endDate).format('YYYY-MM-DD')} onChange={evt => this.updateendDate(evt)} />
+            value={this.state.endDate} onChange={this.updateendDate} />
           </div>
 
           <div class="comment">
             <textarea rows="10" cols="50" id="content" name="content"
-            value={this.state.comment} onChange={evt => this.updatecomment(evt)} ></textarea>
+            value={this.state.comment} onChange={this.updatecomment} ></textarea>
           </div>
           <div class="importance">
             <div class="stars">
@@ -77,46 +132,4 @@ export class Add extends React.Component {
     </div>
         );
   }
-  send = function () {
-    fetch('http://localhost:8080/addevents', { method: 'POST', body: JSON.stringify({
-      title:  this.state.eventName,
-      start:  this.state.startDate,
-      end:    this.state.endDate
-    }) })
-    .then(function() {
-      window.location.hash = '#main';
-    });
-
-  }
-
-  updatestartDate = function(evt) {
-    this.setState({
-      startDate: evt.target.value
-    });
-  }
-  updateendDate = function(evt) {
-    this.setState({
-      endDate: evt.target.value
-    });
-  }
-  updatestartTime = function(evt) {
-    this.setState({
-      startTime: evt.target.value
-    });
-  }
-  updatecomment = function(evt) {
-    this.setState({
-      comment: evt.target.value
-    });
-  }
-  updateeventName = function(evt) {
-    this.setState({
-      eventName: evt.target.value
-    });
-  }
-  updateendTime = function(evt) {
-      this.setState({
-        endTime: evt.target.value
-      });
-    }
 };
