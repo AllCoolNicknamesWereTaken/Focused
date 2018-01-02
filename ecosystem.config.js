f37811b25ff7e2a09b21b5f540a1b3b0ce62ab6b
wojtekw92 <wwozniak92@gmail.com>
2018-01-03 00:52:45 +0100

    updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       user : "oliwia",
       host : "oliwia.jelocartel.com",
       ref  : "origin/master",
-      repo : "git@github.com:wojtekw92/Focused.git",
+      repo : "https://github.com/wojtekw92/Focused.git",
       path : "/srv/oliwia",
       "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
     },
