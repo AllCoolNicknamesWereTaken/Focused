4b8482c9f71b223b8de96ecd8a2d2dab2265f9c6
AllCoolNicknamesWereTaken <oliwia_bar@wp.pl>
2018-01-11 16:02:13 +0100

    login works remotely (fixes2)

--- src/FbButton/index.js
+++ src/FbButton/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './style.css';
 
 export function FbButton(props) {
-  var apiAddress = window.location.
+  var apiAddress = window.location.protocol + '//' + window.location.hostname + ':8080/authfacebook';
   return (
-    <a href={window.location.protocol + '//' + window.location.hostname + ':8080/authfacebook'}>
+    <a href={apiAddress}>
     <button class="loginBtn loginBtn--facebook" >
       Zaloguj przez Facebook
     </button>
